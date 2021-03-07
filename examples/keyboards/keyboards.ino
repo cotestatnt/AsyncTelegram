3f58c9bddf4543fb8f76355e7cf1e4f6735b214d
Tolentino Cotesta <cotestatnt@yahoo.com>
2021-03-07 13:16:23 +0100

    Added setClock() instruction
    To ensure certificate validation, WiFiClientSecure needs time updated

--- examples/keyboards/keyboards.ino
+++ examples/keyboards/keyboards.ino
@@ -74,6 +74,10 @@ void setup() {
     delay(500);
   }
 
+  // To ensure certificate validation, WiFiClientSecure needs time updated
+  // myBot.setInsecure(false);
+  myBot.setClock("CET-1CEST,M3.5.0,M10.5.0/3");
+  
   // Set the Telegram bot properies
   myBot.setUpdateTime(1000);
   myBot.setTelegramToken(token);
