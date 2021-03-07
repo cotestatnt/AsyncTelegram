53749283a7b59d16c960df9c26013cc9d93b522f
Tolentino Cotesta <cotestatnt@yahoo.com>
2021-03-07 13:18:51 +0100

    Added setClock() instruction
    // To ensure certificate validation, WiFiClientSecure needs time updated

--- examples/sendPhotoESP32/sendPhoto_SPIFFS/sendPhoto_SPIFFS.ino
+++ examples/sendPhotoESP32/sendPhoto_SPIFFS/sendPhoto_SPIFFS.ino
@@ -142,6 +142,10 @@ void setup() {
   Serial.printf("Total space: %10u\n", SPIFFS.totalBytes());
   Serial.printf("Free space: %10u\n", SPIFFS.totalBytes() - SPIFFS.usedBytes());
   listDir(SPIFFS, "/", 0);
+  
+  // To ensure certificate validation, WiFiClientSecure needs time updated
+  // myBot.setInsecure(false);
+  myBot.setClock("CET-1CEST,M3.5.0,M10.5.0/3");
 
   // Set the Telegram bot properies
   myBot.setUpdateTime(1000);
